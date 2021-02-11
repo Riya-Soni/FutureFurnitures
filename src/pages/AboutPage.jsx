0e983e509f8ee628696f939aa6ac782f0b7a97e3
Riya-Soni <riyasoni410@gmail.com>
2021-02-11 15:10:04 +0530

    First Commit

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import styled from 'styled-components';
+import { PageHero } from '../components';
+import aboutImg from '../assets/hero-bcg.jpeg';
+
+const AboutPage = () => {
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="desk" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Furniture, household equipment, usually made of wood, metal, plastics, 
+            marble, glass, fabrics, or related materials and having a variety of 
+            different purposes. Furniture ranges widely from the simple pine 
+            chest or stick-back country chair to the most elaborate marquetry 
+            work cabinet or gilded console table. The functional and decorative 
+            aspects of furniture have been emphasized more or less throughout 
+            history according to economics and fashion.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
+
+const Wrapper = styled.section`
+  display: grid;
+  gap: 4rem;
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    height: 500px;
+    object-fit: cover;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+    color: var(--clr-grey-5);
+    font-size: 1rem;
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`;
+export default AboutPage;
---
+++ src/pages/CheckoutPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+import { PageHero } from '../components';
+
+const CheckoutPage = () => {
+  return (
+    <main>
+      <PageHero title="checkout" />
+      <Wrapper className="page">
+        <section className="section section-center">
+          <h1>checkout here...if this was real</h1>
+        </section>
+      </Wrapper>
+    </main>
+  );
+};
+const Wrapper = styled.div``;
+export default CheckoutPage;
